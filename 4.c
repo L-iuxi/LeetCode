484d846cbee79159f811b7155c3d3bd3af59f3ce
liuxgit commit <915988179@qq.com>
2024-11-23 20:34:06 +0800

    t

--- 4.c
+++ 4.c
@@ -1,55 +1,22 @@
-#include<stdio.h>
-#include<math.h>
-int is_prime(int num) {//判断是否为素数
-    if (num <= 1) 
-        return 0;
-    if (num == 2) 
-        return 1;  
-    if (num % 2 == 0) 
-        return 0;  
-    for (int i = 3; i <= sqrt(num); i += 2) 
-    {  
-        if (num % i == 0) 
-            return 0;  
-    }
-    return 1;
-}
-int yesno(int n)
-{
-    int t = 1;
-    int sum = 0;
-    if(is_prime(n))
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    
+    if(headA == NULL || headB == NULL)
     {
-        while(n / 10 != 0)
-        {
-            t++;
-        }
-        for(int i = 0;i < t;i++)
-        {
-            
-            sum += n % 10;
-            n = n / 10;
-        }
-        if(is_prime(sum))
-        {
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }else{
-        return 0;
+        return NULL;
     }
-}
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    if(yesno(n))
+    struct ListNode *pa = headA,*pb = headB;
+    while(pa!= pb)
     {
-        printf("yes");
-    }else{
-        printf("no");
+        pa=pa == NULL ? headB : pa->next;
+        pb=pb == NULL ? headA :pb->next;
+
     }
-    return 0;
+    return pa;
 }
