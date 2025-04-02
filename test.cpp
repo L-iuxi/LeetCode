8f13965445ccdc0f3ada889a692c5ffe69e725a2
liuxgit commit <915988179@qq.com>
2025-04-02 20:47:35 +0800

    test.cpp

--- test.cpp
+++ test.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
 #include <string>
-//#include <stdio.h>
+
 using namespace std;
 
 class test{
     public:
     int a;
-//    test()
-//    {
-//     cout << "222" <<endl;
-//    }
-//      test(const test& t)
-//      {
-//         cout << "111" << endl;
-//      }
-//      ~test();
-void func(int a)
-{
-    this->a = a;
-}
-};
-// test :: ~test()
-// {
-//     cout << "333" << endl;
-// } 
+    test();
+ };
+ test :: test() :a(0)
+ {
+    cout << a << endl;
+ }
 
 int main()
 {
-    test t;
-    t.func(10);
-    cout << t.a << endl;
-    //test b = a;
+   test t;
+
 
 
 }
