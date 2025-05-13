b4ac6b68aa99e51310493fa5132b22bf5294e179
liuyuxi <915988179@qq.com>
2025-05-13 20:24:47 +0800

    17

--- 17.c
+++ 17.c
@@ -1,39 +1,20 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-
- int findm(int *arr,int len,int start,int m)
- {
-    for(int i = start;i < len;i++)
+bool isPalindrome(int x) {
+   if(x < 0 || (x % 10 == 0 && x != 0))
+   {
+    return false;
+   }
+    int num = 0;
+    int start = x;
+    while(start > num)
     {
-        if(arr[i] > m)
-        {
-            return arr[i];
-        }
+        int index = start % 10;
+        num =num *10 + index;
+        start /= 10;
     }
-    return -1;
- }
-
- int findnum(int *arr,int m,int len)
- {
-    for(int i = 0;i < len;i++)
+    if(num  ==start || num / 10 == start)
     {
-        if(arr[i] == m)
-        {
-            return i;
-        }
+        return true;
+    }else{
+        return false;
     }
-    return -1;
- }
-int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
-    int *result= (int*)malloc(sizeof(int ) * nums1Size);
-    *returnSize = nums1Size;
-    int top = 0;
-    for(int i = 0;i < nums1Size;i++)
-    {
-        int point = findnum(nums2,nums1[i],nums2Size);
-        result[top] = findm(nums2,nums2Size,point,nums1[i]);
-            top++;
-    }
-    return result;
 }
