5268abf3de308d6e810d2a32927b0ba38ab4c06e
liuyuxi <915988179@qq.com>
2025-05-15 19:32:53 +0800

    5.15

--- 17.c
+++ 17.c
@@ -1,54 +1,63 @@
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
-void swap(int* a,int* b)
-{
-    int t;
-    t = *a;
-    *a = *b;
-    *b = t;
-}
-int factorial(int n) {
-    int fact = 1;
-    for (int i = 1; i <= n; i++) {
-        fact *= i;
-    }
-    return fact;
-}
 
- void back(int *arr,int n,int start,int **result,int *returnColumnSizes,int *returnSize)
- {
-    if(start == n)
+void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
+    bool firstrow = false;
+    bool firstcol = false;
+
+    for(int i = 0;i < matrixSize;i++)
     {
-        result[*returnSize] = (int*)malloc(sizeof(int) * n);
-        for(int i = 0;i < n;i++)
+        if(matrix[i][0] == 0)
         {
-            result[*returnSize][i] = arr[i];
-           
+            firstcol = true;
+            break;
         }
-         returnColumnSizes[*returnSize] = n;
-         (*returnSize)++;
-        return;
     }
-    for(int i = start;i < n;i++)
+
+    for(int i = 0;i < matrixColSize[0];i++)
     {
-        swap(&arr[start],&arr[i]);
-
-        back(arr,n,start+1,result,returnColumnSizes,returnSize);
-
-        swap(&arr[start],&arr[i]);
+        if(matrix[0][i] == 0)
+        {
+            firstrow = true;
+            break;
+        }
     }
- }
-int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
 
-    int fact = factorial(numsSize);
-    int **result = (int**)malloc(sizeof(int*) * fact);
-    *returnColumnSizes = (int*)malloc(sizeof(int) * fact); 
+    for(int i = 1;i < matrixSize;i++)
+    {
+        for(int j = 1;j < matrixColSize[i];j++)
+        {
+            if(matrix[i][j] == 0)
+            {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
 
-     *returnSize = 0;
-    back(nums,numsSize,0,result,*returnColumnSizes,returnSize);
+    
+    for(int i = 1;i < matrixSize;i++)
+    {
+        for(int j = 1;j < matrixColSize[i];j++)
+        {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0)
+            {
+               matrix[i][j] = 0;
+            }
+        }
+    }
 
-    return result;
+    if(firstcol)
+    {
+        for(int i = 0; i < matrixSize;i++)
+        {
+            matrix[i][0] = 0;
+        }
+    }
+
+    if(firstrow)
+    {
+        for(int i = 0; i < matrixColSize[0];i++)
+        {
+            matrix[0][i] = 0;
+        }
+    }
 }
