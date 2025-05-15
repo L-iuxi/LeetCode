bca34f99fbc01f24dc5fc29e583978bd5c92fe7c
liuyuxi <915988179@qq.com>
2025-05-15 20:34:26 +0800

    5.15

--- 17.c
+++ 17.c
@@ -1,63 +1,28 @@
-
-void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
-    bool firstrow = false;
-    bool firstcol = false;
-
-    for(int i = 0;i < matrixSize;i++)
-    {
-        if(matrix[i][0] == 0)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count = 1;
+        int k = 1;
+        if(nums.empty())
         {
-            firstcol = true;
-            break;
+            return 0;
         }
-    }
-
-    for(int i = 0;i < matrixColSize[0];i++)
-    {
-        if(matrix[0][i] == 0)
+        
+        for(int i = 1;i < nums.size();i++)
         {
-            firstrow = true;
-            break;
-        }
-    }
-
-    for(int i = 1;i < matrixSize;i++)
-    {
-        for(int j = 1;j < matrixColSize[i];j++)
-        {
-            if(matrix[i][j] == 0)
+            if(nums[i] == nums[i-1])
             {
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
+                count++;
+            }else{
+                count = 1;
+            }
+
+            if(count <= 2)
+            {
+                nums[k] = nums[i];
+                k++;
             }
         }
+        return k;
     }
-
-    
-    for(int i = 1;i < matrixSize;i++)
-    {
-        for(int j = 1;j < matrixColSize[i];j++)
-        {
-            if(matrix[i][0] == 0 || matrix[0][j] == 0)
-            {
-               matrix[i][j] = 0;
-            }
-        }
-    }
-
-    if(firstcol)
-    {
-        for(int i = 0; i < matrixSize;i++)
-        {
-            matrix[i][0] = 0;
-        }
-    }
-
-    if(firstrow)
-    {
-        for(int i = 0; i < matrixColSize[0];i++)
-        {
-            matrix[0][i] = 0;
-        }
-    }
-}
+};
