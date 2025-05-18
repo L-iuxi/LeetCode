66c2cefc55ed553081ef973329ec3758ba1def0d
liuyuxi <915988179@qq.com>
2025-05-18 20:47:05 +0800

    5.18

--- 17.c
+++ 17.c
@@ -1,46 +1,63 @@
 /**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ * Note: The returned array must be malloced, assume caller calls free().
  */
-int* path;
-int pathTop;
-int** ans;
-int ansTop;
-int cnt[8];//标记path中是否已有此索引值，这也是同46题不同点
-void backTracking(int* nums,int numsSize,int startIndex,int** returnColumnSizes){
-    if(pathTop==numsSize){
-        (*returnColumnSizes)[ansTop] = pathTop;
-        ans[ansTop] = (int*)malloc(sizeof(int)*pathTop);
-        for(int i = 0;i<pathTop;i++){//装入子集
-            ans[ansTop][i] = path[i];
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
+    int left = 0,right = *matrixColSize -1,top = 0,bottom = matrixSize - 1;
+    *returnSize = 0;
+    int *result = (int*)malloc(sizeof(int) * (*matrixColSize) * matrixSize);
+
+    if((*matrixColSize) == 0 || matrixSize == 0)
+    {
+        return result;
+    }
+    if((*matrixColSize) == 1)
+    {
+        for(int i = 0;i < matrixSize;i++)
+        {
+            result[(*returnSize)++] = matrix[i][0];
         }
-        ansTop++;
-        return;
+        return result;
     }
-    int cnt_1[21];//同一层相同元素只能读一次
-    memset(cnt_1,0,sizeof(cnt_1));
-    //递归组合
-    for(int i = startIndex;i<numsSize;i++){
-        if(cnt[i]||cnt_1[nums[i]+10]) continue;//此索引本轮已访问过或此值在此层已访问过
-        path[pathTop++] = nums[i];
-        cnt[i] = 1;//本组合相应索引位置已访问
-        cnt_1[nums[i]+10] = 1;//本层相同元素已访问
-        backTracking(nums,numsSize,0,returnColumnSizes);
-        cnt[i] = 0;//退出路径则解除标记
-        pathTop--;
+
+    if((matrixSize) == 1)
+    {
+        for(int i = 0;i < (*matrixColSize);i++)
+        {
+            result[(*returnSize)++] = matrix[0][i];
+        }
+        return result;
     }
-}
-int** permuteUnique(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    memset(cnt,0,sizeof(cnt));
-    path = (int*)malloc(sizeof(int)*8);//开到最大
-    ans = (int**)malloc(sizeof(int*)*10000);
-    *returnColumnSizes = (int*)malloc(sizeof(int)*10000);
-    pathTop = 0;
-    ansTop = 0;
-    //从0开始递归
-    backTracking(nums,numsSize,0,returnColumnSizes);
-    *returnSize = ansTop;
-    free(path);
-    return ans;
+    while(top <= bottom && left <= right)
+    {
+        for(int i = left;i <= right;i++)
+        {
+            result[(*returnSize)++] = matrix[top][i];
+        }
+        top++;
+        for(int i = top;i <= bottom;i++)
+        {
+            result[(*returnSize)++] = matrix[i][right];
+        }
+     right--;
+
+        if(top <= bottom)
+        {
+            for(int i = right;i >= left;i--)
+            {
+                result[(*returnSize)++] = matrix[bottom][i];
+            }
+            bottom--;
+        }
+
+        if(left <= right)
+        {
+            for(int i = bottom;i >= top;i--)
+            {
+                result[(*returnSize)++] = matrix[i][left];
+            }
+            left++;
+        }
+
+    }
+    return result;
 }
