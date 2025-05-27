0513805a9873ac7e49d6ef140836934173bde3d9
liuyuxi <915988179@qq.com>
2025-05-27 20:35:51 +0800

    5.27

--- 17.c
+++ 17.c
@@ -1,63 +1,13 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
-    int left = 0,right = *matrixColSize -1,top = 0,bottom = matrixSize - 1;
-    *returnSize = 0;
-    int *result = (int*)malloc(sizeof(int) * (*matrixColSize) * matrixSize);
-
-    if((*matrixColSize) == 0 || matrixSize == 0)
-    {
-        return result;
-    }
-    if((*matrixColSize) == 1)
-    {
-        for(int i = 0;i < matrixSize;i++)
-        {
-            result[(*returnSize)++] = matrix[i][0];
-        }
-        return result;
-    }
-
-    if((matrixSize) == 1)
-    {
-        for(int i = 0;i < (*matrixColSize);i++)
-        {
-            result[(*returnSize)++] = matrix[0][i];
-        }
-        return result;
-    }
-    while(top <= bottom && left <= right)
-    {
-        for(int i = left;i <= right;i++)
-        {
-            result[(*returnSize)++] = matrix[top][i];
-        }
-        top++;
-        for(int i = top;i <= bottom;i++)
-        {
-            result[(*returnSize)++] = matrix[i][right];
-        }
-     right--;
-
-        if(top <= bottom)
-        {
-            for(int i = right;i >= left;i--)
-            {
-                result[(*returnSize)++] = matrix[bottom][i];
-            }
-            bottom--;
-        }
-
-        if(left <= right)
-        {
-            for(int i = bottom;i >= top;i--)
-            {
-                result[(*returnSize)++] = matrix[i][left];
-            }
-            left++;
-        }
-
-    }
-    return result;
+#define max(a,b) ((a) > (b) ? (a):(b))
+#define min(a,b) ((a) > (b) ? (b):(a))
+int maxProfit(int* prices, int pricesSize) {
+  int result = 0;
+  int min_p = prices[0];
+  for(int i = 0;i < pricesSize;i++)
+  {
+    int p = prices[i];
+    result= max(p-min_p,result);
+    min_p = min(min_p,p);
+  }
+  return result;
 }
