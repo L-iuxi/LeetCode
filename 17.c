4b9f6bb83d98981a264ce2555f6d527ab43cadc1
liuyuxi <915988179@qq.com>
2025-05-28 19:10:54 +0800

    5.28

--- 17.c
+++ 17.c
@@ -1,13 +1,37 @@
-#define max(a,b) ((a) > (b) ? (a):(b))
-#define min(a,b) ((a) > (b) ? (b):(a))
-int maxProfit(int* prices, int pricesSize) {
-  int result = 0;
-  int min_p = prices[0];
-  for(int i = 0;i < pricesSize;i++)
-  {
-    int p = prices[i];
-    result= max(p-min_p,result);
-    min_p = min(min_p,p);
-  }
-  return result;
-}
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>result;
+        unordered_set<int> set;
+        if(nums1.size() > nums2.size())
+        {
+        for(int i = 0;i < nums1.size();i++)
+        {
+            for(int j = 0 ;j < nums2.size();j++)
+            {
+                if(nums2[j] == nums1[i])
+                {
+                    set.insert(nums1[i]);
+                    
+                }
+            }
+        }
+        }else{
+               for(int i = 0;i < nums2.size();i++)
+        {
+            for(int j = 0 ;j < nums1.size();j++)
+            {
+                if(nums1[j] == nums2[i])
+                {
+                    set.insert(nums2[i]);
+                    
+                }
+            }
+        }
+        }
+        result.insert(result.end(),set.begin(),set.end());
+    return result;
+    }
+
+    
+};
