5dd0c66e953bf2bdc3f2fcc6ead3cdce9e9fb922
liuyuxi <915988179@qq.com>
2025-07-10 09:48:16 +0800

    7.10

--- 17.c
+++ 17.c
@@ -1,65 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+int strStr(char* haystack, char* needle) {
+    int i = 0;
+    int result = -1;
+    int len = strlen(needle);
+    while(haystack[i] != '\0')
+    {
+        if(result != -1)
+        {
+            break;
+        }
+        if(haystack[i] == needle[0])
+        {
+            int h = i;
+            result = i;
+            for(int j = 0;j < len;j++)
+            {
 
-char* addBinary(char* a, char* b) {
-    int len1 = strlen(a);
-    int len2 = strlen(b);
-
-   
-    char *arr1 = (char*)malloc(sizeof(char) * (len1 + 1)); 
-    char *arr2 = (char*)malloc(sizeof(char) * (len2 + 1));
-    
-    int ind1 = len1 - 1, ind2 = len2 - 1;
-    for (int i = 0; i < len1; i++) {
-        arr1[i] = a[ind1--];
-    }
-    for (int i = 0; i < len2; i++) {
-        arr2[i] = b[ind2--];
-    }
-
-   
-    int max_len = (len1 > len2 ? len1 : len2) + 2;
-    char *result = (char*)malloc(sizeof(char) * max_len);
-    int w = 0;
-    int carry = 0;
-
-    while (w < len1 || w < len2 || carry) {
-        int sum = carry;
+                if(haystack[h] != needle[j])
+                {
+                    result = -1;
+                    break;
+                }
+                h++;
+            }
         
-        if (w < len1 && arr1[w] == '1') {
-            sum += 1;
         }
-        if (w < len2 && arr2[w] == '1') {
-            sum += 1;
-        }
-
-        if (sum % 2 == 1) {
-            result[w] = '1';
-        } else {
-            result[w] = '0';
-        }
-
-        carry = sum / 2;
-        w++;
+        i++;
     }
-
-    result[w] = '\0'; 
-
-    
-    char *end = (char*)malloc(sizeof(char) * (w + 1));
-    int count = w - 1;
-    for (int i = 0; i < w; i++) {
-        end[i] = result[count--];
-    }
-    end[w] = '\0';
-
-   
-    free(arr1);
-    free(arr2);
-    free(result);
-
-    return end;
+    return result;
 }
-
-
