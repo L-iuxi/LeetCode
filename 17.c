1933116fbfb8b9a7c760f8463b2cdbc6e0ecae9c
liuyuxi <915988179@qq.com>
2025-07-12 09:47:59 +0800

    7.12

--- 17.c
+++ 17.c
@@ -1,35 +1,20 @@
-int binary(char *s, int l, int x) {
-    int r = l;
-    for (; x; x >>= 1) {
-        s[r++] = '0' + (x & 1);
-    }
-    for (int i = l, j = r - 1; i < j; i++, j--) {
-        char t = s[i];
-        s[i] = s[j];
-        s[j] = t;
-    }
-    return r;
-}
-int toint(char *s,int l,int len)
+int maxab(int a,int b)
 {
-    int sum = 0;
-    for(;l<len;l++)
+    if(a>b)
     {
-        sum = sum*10 + s[l] - '0';
+        return a;
+    }
+    return b;
+}
+int subarraySum(int* nums, int numsSize) {
+    int sum = 0;
+    for(int i = 0;i < numsSize;i++)
+    {
+        int start = maxab(0,i-nums[i]);
+        for(int j = start;j <= i;j++)
+        {
+            sum += nums[j];
+        }
     }
     return sum;
-}
-char* convertDateToBinary(char* date) {
-    int year = toint(date,0,4);
-    int month = toint(date,5,7);
-    int day = toint(date,8,10);
-
-char *res = (char *)calloc(128, 1);
-    int i = binary(res, 0, year);
-    res[i++] = '-';
-    i = binary(res, i, month);
-    res[i++] = '-';
-    i = binary(res, i, day);
-    return res;
-
 }
