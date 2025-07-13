be92547a5bccf3bfbc360349b556d36889e48e43
liuyuxi <915988179@qq.com>
2025-07-13 15:29:23 +0800

    7.12

--- 17.c
+++ 17.c
@@ -1,17 +1,24 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* stableMountains(int* height, int heightSize, int threshold, int* returnSize) {
-    int *result = (int*)malloc(sizeof(int) * 100);
-    (*returnSize) = 0;
-    for(int i = 1;i < heightSize;i++)
+int func(int a,int b)
+{
+    if(a > b)
     {
-        int t = i-1;
-        if(height[i-1] > threshold)
-        {
-            result[*returnSize] = i;
-            (*returnSize)++;
-        }
+        return a - b;
     }
-    return result;
+        return b - a;
+    
+}
+int findClosest(int x, int y, int z) {
+    int a = func(x,z);
+    int b = func(y,z);
+    
+    if(a < b)
+    {
+        return 1;
+    }else if(b < a)
+    {
+        return 2;
+    }
+        return 0;
+    
+  
 }
