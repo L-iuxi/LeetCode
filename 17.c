e7fda15095503688ffbd03cf704a6267dac9c870
liuyuxi <915988179@qq.com>
2025-07-14 21:56:23 +0800

    7.14

--- 17.c
+++ 17.c
@@ -1,24 +1,11 @@
-int func(int a,int b)
-{
-    if(a > b)
+int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target) {
+    int sum = 0;
+    for(int i = 0;i < hoursSize;i++)
     {
-        return a - b;
+        if(hours[i] >= target)
+        {
+            sum++;
+        }
     }
-        return b - a;
-    
-}
-int findClosest(int x, int y, int z) {
-    int a = func(x,z);
-    int b = func(y,z);
-    
-    if(a < b)
-    {
-        return 1;
-    }else if(b < a)
-    {
-        return 2;
-    }
-        return 0;
-    
-  
+    return sum;
 }
