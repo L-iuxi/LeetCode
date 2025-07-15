d68299dffa354d96148d48777e37ab96925fe667
liuyuxi <915988179@qq.com>
2025-07-15 16:55:18 +0800

    7,15

--- 17.c
+++ 17.c
@@ -1,11 +1,20 @@
-int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target) {
-    int sum = 0;
-    for(int i = 0;i < hoursSize;i++)
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
+   int *result = (int*)malloc(sizeof(int) *100);
+   *returnSize = 0;
+   for(int i = 0;i < numsSize;i++)
+   {
+    for(int j = i+1;j < numsSize;j++)
+   {
+    if(nums[i] == nums[j])
     {
-        if(hours[i] >= target)
-        {
-            sum++;
-        }
+        result[*returnSize] = nums[i];
+        (*returnSize)++;
+        break;
     }
-    return sum;
+   }
+   } 
+   return result;
 }
