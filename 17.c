d0e71a7408ce717855a3e4cefccdd82f38c0f536
liuyuxi <915988179@qq.com>
2025-07-23 21:33:59 +0800

    7.23

--- 17.c
+++ 17.c
@@ -1,16 +1,41 @@
-int countTestedDevices(int* batteryPercentages, int batteryPercentagesSize) {
-    int count = 0;
-   for(int i = 0;i <batteryPercentagesSize;i++)
-   {
-    batteryPercentages[i]-=count;
-    if(batteryPercentages[i] < 0)
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+double culculate(int a,int b,int c,int d)
+{
+    double len =1.0 * abs((a-c)*(a-c)+(b-d)*(b-d));
+    len = sqrt(len);
+    return len;
+}
+bool confine(double len,int r)
+{
+    if(len <= 1.0*r)
     {
-        batteryPercentages[i] = 0;
+        return true;
     }
-    if(batteryPercentages[i] > 0)
+    return false;
+}
+int* countPoints(int** points, int pointsSize, int* pointsColSize, int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
+    int *result = (int*)malloc(sizeof(int) *1008);
+    memset(result, 0, sizeof(int) * 1008);
+    (*returnSize) = 0;
+    for(int j = 0;j < queriesSize;j++)
     {
-        count++;
+        int a = queries[j][0];
+        int b = queries[j][1];
+        int r = queries[j][2];
+    for(int i = 0;i < pointsSize;i++)
+    {
+        int c = points[i][0];
+        int d = points[i][1];
+        double len = culculate(a,b,c,d);
+        if(confine(len,r))
+    {
+        result[(*returnSize)]++;
     }
-   } 
-   return count;
+    }
+    
+    (*returnSize)++;
+    }
+    return result;
 }
